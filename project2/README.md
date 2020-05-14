abc5b927094cd82ddb2e5e8650bc8fe99eac29ae
Merissa Lissade <mlissade@gradcenter.cuny.edu>
2020-05-14 17:35:37 -0400

    project 2 added

--- project2/README.md
+++ project2/README.md
@@ -1,3 +1,7 @@
 # Project 2 - Narrative Visualization
 
 ## Prospectus
+
+For this project, I will be creating a visualization that shows the Gender Pay Gap in the US. My visualization is based off a similar visualization that uses the same [dataset](https://docs.google.com/spreadsheets/d/1Qih5qBcuTntLbx7G7BzunRSOgGD0b_zc07sTzqiKGn4/edit#gid=0) from the website [Information is Beautiful](https://informationisbeautiful.net/data/).
+
+## Sketches and Mockups
--- project2/graph1.js
+++ project2/graph1.js
@@ -3,26 +3,27 @@ d3.csv(
   d3.autoType
 ).then(data => {
   console.log(data);
+  const sortedData = data.reverse();
 
   const width = window.innerWidth * 0.9,
-    height = window.innerHeight,
+    height = window.innerHeight * 2,
     paddingInner = 0.2,
-    margin = { top: 20, bottom: 40, left: 40, right: 40 };
+    margin = { top: 20, bottom: 40, left: 160, right: 40 };
 
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d["Pay gap ($)"])])
-    .range([width - margin.left, margin.right]);
+    .range([margin.left, width - margin.right]);
 
   const yScale = d3
     .scaleBand()
-    .domain(data.map(d => d.Occupation))
+    .domain(sortedData.map(d => d.Occupation))
     .range([height - margin.bottom, margin.top])
     .paddingInner(paddingInner);
 
   const yAxis = d3.axisLeft(yScale).ticks(data.length);
 
-  // const xAxis = d3.axisBottom(xScale).ticks(d => d["Pay gap ($)"]);
+  const xAxis = d3.axisBottom(xScale); //.ticks(d => d["Pay gap ($)"]);
 
   const svg = d3
     .select("#d3-container")
@@ -35,12 +36,12 @@ d3.csv(
     .data(data)
     .join("rect")
     .attr("y", d => yScale(d.Occupation))
-    .attr("x", 0, d => xScale(d["Pay gap ($)"]))
-    .attr("width", d => width - margin.left - xScale(d["Pay gap ($)"]))
-    .attr(
-      "transform",
-      `translate(160, ${(height - margin.bottom, margin.top)})`
-    )
+    .attr("x", margin.left)
+    .attr("width", d => xScale(d["Pay gap ($)"]))
+    //.attr(
+    //   "transform",
+    // `translate(160, ${(height - margin.bottom, margin.top)})`
+    //)
     .attr("height", yScale.bandwidth())
     .attr("fill", "darkmagenta");
 
@@ -49,19 +50,22 @@ d3.csv(
     .data(data)
     .join("text")
     .attr("class", "label")
-    // this allows us to position the text in the center of the bar
-    .attr("x", 0, d => xScale(d["Pay gap ($)"]))
-    .attr("y", d => yScale(d.Occupation) + yScale.bandwidth())
+    //positioning the text in the center of the bar
+    .attr("x", margin.left)
+    .attr("y", d => yScale(d.Occupation))
     .text(d => d["Pay gap ($)"])
-    .attr("dx", "160");
+    .attr("dx", "10")
+    .attr("dy", ".8em");
 
   svg
     .append("g")
-    .attr("class", "axis")
-    .attr(
-      "transform",
-      `translate(160, ${(height - margin.bottom, margin.top)})`
-    )
-    //.call(xAxis)
+    .attr("class", "y-axis")
+    .attr("transform", `translate(${margin.left}, 0)`)
     .call(yAxis);
+
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
 });
